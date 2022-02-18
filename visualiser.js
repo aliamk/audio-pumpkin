f56d502c2fa0801d21fc2da65f66f7998b3ca50a
unknown <alia.azarian@gmail.com>
2022-02-18 23:28:51 +0000

    rotated the bar lines

--- visualiser.js
+++ visualiser.js
@@ -5,41 +5,46 @@ const main = () => {
     canvas.height = window.innerHeight
 
     class Bar {
-        constructor(x, y, width, height, color) {
+        constructor(x, y, width, height, color, index) {
             this.x = x;
             this.y = y;
             this.width = width;
             this.height = height;
             this.color = color;
+            this.index = index;
         }
         update(micInput) {
-            this.height = micInput * 8000
+            this.height = micInput * 1000
         }
 
         draw(context) {
             // context.fillStyle = this.color;
             context.strokeStyle = this.color;
-            context.fillRect(this.x, this.y, this.width, this.height)
+            context.save()
+            // context.fillRect(this.x, this.y, this.width, this.height)
+            context.translate(canvas.width * 0.5, canvas.height * 0.5)
+            context.rotate(this.index * 0.01)
             context.beginPath(); // Need this to stop bars forming one big shape
-            context.moveTo(this.x, this.height); // starting coordinates of the bar 
-            context.lineTo(this.x, this.y); // ending coordinates of the bar
+            //context.moveTo(this.x, this.height); // starting coordinates of the bar 
+            context.moveTo(0, 0);
+            context.lineTo(0, this.height); // ending coordinates of the bar
             context.stroke()
+            context.restore()
         }
     }
 
     const microphone = new Microphone();
 
     let bars = []
-    let barWidth = canvas.width / 256
+    let barWidth = canvas.width / 256;
     // Create an instance of a bar for each audio sample
     const createBars = () => {
         for (let i = 0; i < 256; i++) {
             // let color = 'hsl(0, 100%, 50%)'
             let color = 'hsl(' + i * 2 + ', 100%, 50%)';
-            bars.push(new Bar(i * barWidth, canvas.height / 2, 1, 100, color))
+            bars.push(new Bar(i * barWidth, canvas.height * 0.5, 1, 20, color, i))
         }
     }
-    console.log(bars)
     createBars()
 
     const animate = () => {
